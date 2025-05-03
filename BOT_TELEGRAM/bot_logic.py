114ba0ce53eae0b848d1fcfb81b964d829dd4d11
xem1l1 <sosnovskaemilija8@gmail.com>
2025-05-03 13:27:19 +0300

    Delete BOT_TELEGRAM directory

--- BOT_TELEGRAM/bot_logic.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>123456789"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
-    
