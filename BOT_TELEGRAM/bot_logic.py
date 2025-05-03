21c58933a89c730f3a2016e7a1ee4fd54cd7ce7f
xem1l1 <sosnovskaemilija8@gmail.com>
2025-05-03 13:25:58 +0300

    Add files via upload

---
+++ BOT_TELEGRAM/bot_logic.py
@@ -0,0 +1,9 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+    
